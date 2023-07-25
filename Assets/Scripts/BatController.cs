f662bb5efcc45d40d9759c06c2c7b2d929798466
kim-seungbeom <102759012+kim-seungbeom@users.noreply.github.com>
2023-07-25 16:40:04 +0900

    bat 수정
    bat이 바닥에 닿아야만 바뀌도록

--- Assets/Scripts/BatController.cs
+++ Assets/Scripts/BatController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BatController : MonoBehaviour
 {
     private bool isBat = false;
+    private bool changing = false;
     private PlayerController pc;
     // Start is called before the first frame update
 
@@ -22,12 +23,15 @@ public class BatController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B))
+        if (pc.isJump == false&&changing==false)
         {
-            isBat = !isBat;
-            pc.rb.gravityScale = -pc.rb.gravityScale;
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                isBat = !isBat;
+                changing = true;
+                pc.rb.gravityScale = -pc.rb.gravityScale;
+            }
         }
-
         if (isBat && Input.GetKeyDown(KeyCode.Space) && !pc.isJump)
         {
             Debug.Log("Batjump");
@@ -41,4 +45,11 @@ public class BatController : MonoBehaviour
         pc.rb.AddForce(new Vector2(0f, -pc.jumpForce), ForceMode2D.Impulse);
     }
 
+    private void OnCollisionEnter2D(Collision2D Batcollision)
+    {
+        if (Batcollision.gameObject.CompareTag("Ground"))
+        {
+            changing = false;
+        }
+    }
 }
